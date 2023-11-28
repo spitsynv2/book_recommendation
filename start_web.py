88acd514a794047132234085500bc79e052da67c
spitsynv2 <61032832+spitsynv2@users.noreply.github.com>
2023-11-28 16:49:56 +0100

    Update start_web.py, &country=PL

--- start_web.py
+++ start_web.py
@@ -23,7 +23,7 @@ def get_google_books_info(book_title, target_language='en'):
 
     book_title = book_title.replace(" ", "%20")
 
-    url = f"https://www.googleapis.com/books/v1/volumes?q=intitle:{book_title}&key={GOOGLE_BOOKS_API_KEY}"
+    url = f"https://www.googleapis.com/books/v1/volumes?q=intitle:{book_title}&key={GOOGLE_BOOKS_API_KEY}&country=PL"
     print(url)
 
     response = requests.get(url)
